a55c0589820702dc542d7de3cd024ac700c35559
mariia10 <maboudiak@mail.ru>
2025-03-01 01:35:15 +0700

    data about net adapters begining

--- src/main/java/mariia/budiak/practices/controller/GetLocalMashineInfoController.java
+++ src/main/java/mariia/budiak/practices/controller/GetLocalMashineInfoController.java
@@ -1,12 +1,19 @@
 package mariia.budiak.practices.controller;
 
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.WinReg;
+import com.sun.jna.ptr.IntByReference;
 import mariia.budiak.practices.service.RegistryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.sun.jna.platform.win32.IPHlpAPI;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.ptr.IntByReference;
 
+import java.util.ArrayList;
 import java.util.List;
 @RequestMapping("local-mashine-register-info/")
 @RestController
@@ -19,5 +26,26 @@ public class GetLocalMashineInfoController {
     @GetMapping("/userInformation")
     public List<String> getUserInformation(@RequestParam(value = "path", defaultValue = DEFAULT_REGISTRY_PATH) String registryPath) {
        return registryService.getUserInformation(registryPath);
+    }
+
+    @GetMapping("/networkInformation")
+    public List<String> getNetworkInformation() {
+        List<String> devices = new ArrayList<>();
+        String registryPath = "SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}";
+
+        try {
+            String[] subKeys = Advapi32Util.registryGetKeys(WinReg.HKEY_LOCAL_MACHINE, registryPath);
+            for (String subKey : subKeys) {
+                String deviceName = Advapi32Util.registryGetValue(WinReg.HKEY_LOCAL_MACHINE, registryPath + "\\" + subKey, "DriverDesc")!=null?
+                        Advapi32Util.registryGetValue(WinReg.HKEY_LOCAL_MACHINE, registryPath + "\\" + subKey, "DriverDesc").toString(): null;
+                if (deviceName != null) {
+                    devices.add(deviceName);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return devices;
     }
 }
